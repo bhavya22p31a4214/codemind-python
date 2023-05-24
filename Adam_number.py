3cec01901d9f9b01000df870182e1c0ef0d91c17
bhavya  317 <120095918+bhavya22p31a4214@users.noreply.github.com>
2023-05-24 13:17:09 +0530

    n=int(input()) a=n*n s=0 p=0 while n!=0:     r=n%10     n//=10     s=s*10+r     q=s*s while q!=0:     c=q%10     q//=10     p=p*10+c if a==p:     print(True) else:     print(False)

---
+++ Adam_number.py
@@ -0,0 +1,17 @@
+n=int(input())
+a=n*n
+s=0
+p=0
+while n!=0:
+    r=n%10
+    n//=10
+    s=s*10+r
+    q=s*s
+while q!=0:
+    c=q%10
+    q//=10
+    p=p*10+c
+if a==p:
+    print(True)
+else:
+    print(False)
